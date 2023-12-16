ee820d31c92a40db47eea7e9b1f5851b316767ae
vladradu <mevladradu@gmail.com>
2023-12-16 12:20:36 +0200

    solved security over microservices

--- src/main/java/com/sd/devicemanagement/DeviceManagementApplication.java
+++ src/main/java/com/sd/devicemanagement/DeviceManagementApplication.java
@@ -1,12 +1,31 @@
 package com.sd.devicemanagement;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @SpringBootApplication
 public class DeviceManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeviceManagementApplication.class, args);
+	}
+
+	@Value("${usermicroservice.port}")
+	private int user_port;
+	@Value("${usermicroservice.host}")
+	private String user_host;
+	@Bean
+	public WebClient webClient() {
+		String baseUrl = UriComponentsBuilder.fromUriString("http://" + user_host + ":")
+				.port(user_port)
+				.path("/api")
+				.build()
+				.toUriString();
+
+		return WebClient.builder().baseUrl(baseUrl).build();
 	}
 }
